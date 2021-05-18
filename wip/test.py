c0279726b77e78733c891519d5d28d8bc0e1357e
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-17 23:01:00 -0400

    add some test stuff fix some little probs

---
+++ wip/test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+'''
+trying to make a python equivalent for the linux command below
+raspivid -n -ih -t 0 -rot 0 -w 1280 -h 720 -fps 15 -b 1000000 -o - | ncat -lkv4 9090
+'''
+import socket
+import time
+import picamera
+with picamera.PiCamera() as camera:
+	camera.resolution = (640, 480)
+	camera.framerate = 24
+	server_socket = socket.socket()
+	server_socket.bind(('0.0.0.0', 8000))
+	server_socket.listen(0)
+	while True:
+		connection = server_socket.accept()[0].makefile('wb')
+		camera.start_recording(connection, format='h264')
+		try:
+			while True:
+				time.sleep(1)
+		except Exception:
+			pass
+		camera.stop_recording()
+		connection.close()
