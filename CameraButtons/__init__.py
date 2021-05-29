9b1343bead12d286d25b3dc782aa319f6c9bf7f2
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-28 23:59:00 -0400

    add setup.py stuff

---
+++ CameraButtons/__init__.py
@@ -0,0 +1,1 @@
+from CameraButtons import *
---
+++ setup.py
@@ -0,0 +1,12 @@
+from ez_setup import use_setuptools
+
+use_setuptools()
+from setuptools import setup, find_packages
+
+setup(name='CameraButtons',
+      version='1.0.0',
+      author='Aaron Watkins',
+      author_email='ac.watkins80@gmail.com',
+      description='Control a pi camera remotely with REST.',
+      license='MIT',
+      packages=find_packages())
