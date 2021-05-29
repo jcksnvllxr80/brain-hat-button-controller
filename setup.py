f6544feaeb07f36a7c4f08cf49f6c4b8067230a3
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-29 00:43:06 -0400

    try naming the package something else

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup, find_packages
 
-setup(name='CameraButtons',
+setup(name='remote_buttons',
       version='1.0.2',
       author='Aaron Watkins',
       author_email='ac.watkins80@gmail.com',
