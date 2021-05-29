f5f7ef1c78267bfed5238dfd5a2013992d67ae39
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-29 17:07:16 -0400

    make the install install the deps too

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ use_setuptools()
 from setuptools import setup, find_packages
 
 setup(name='remote_buttons',
-      version='1.0.9',
+      version='1.1.0',
       author='Aaron Watkins',
       author_email='ac.watkins80@gmail.com',
       description='Control a pi camera remotely with REST.',
       license='MIT',
-      packages=find_packages())
+      packages=find_packages(),
+      install_requires=[
+          'pyyaml',
+          'flask_cors',
+      ])
