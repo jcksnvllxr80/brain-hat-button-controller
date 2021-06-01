790724a904887a698be13850b8913522bf487849
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-31 21:28:34 -0400

    atttempt to left justify buttons

--- wip/guitkinter_test.py
+++ wip/guitkinter_test.py
@@ -6,7 +6,13 @@ from tkinter import messagebox
 def init(win):
   win.title("Hello World application")
   win.minsize(800, 480)
-  btn.pack()
+  select_btn.pack()
+  photo_btn.pack()
+  up_btn.pack()
+  down_btn.pack()
+  left_btn.pack()
+  right_btn.pack()
+  menu_btn.pack()
 
 # button callbacks
 def photo():
@@ -35,13 +41,14 @@ win = Tk()
 positionRight = 0
 positionDown = 0
 win.geometry("+{}+{}".format(positionRight, positionDown))
-select_btn = Button(win, text="select", command=select)
-photo_btn = Button(win, text="photo", command=photo)
-up_btn = Button(win, text="up", command=up)
-down_btn = Button(win, text="down", command=down)
-left_btn = Button(win, text="left", command=left
-right_btn = Button(win, text="right", command=right)
-menu_btn = Button(win, text="menu", command=menu)
+
+select_btn = Button(win, text="select", command=select, width=10, justify="left")
+photo_btn = Button(win, text="photo", command=photo, width=10, justify="left")
+up_btn = Button(win, text="up", command=up, width=10, justify="left")
+down_btn = Button(win, text="down", command=down, width=10, justify="left")
+left_btn = Button(win, text="left", command=left, width=10, justify="left")
+right_btn = Button(win, text="right", command=right, width=10, justify="left")
+menu_btn = Button(win, text="menu", command=menu, width=10, justify="left")
 
 # initialise and start main lophoto
 init(win)
