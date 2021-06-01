48d5fb8c3754df4bd0f1eed48005aff6faba5fe7
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-31 20:50:21 -0400

    add new tests

---
+++ wip/guizero_test.py
@@ -0,0 +1,11 @@
+# from guizero import App, PushButton
+import guizero
+app = guizero.App(
+    title="My second GUI app", 
+    width=800, 
+    height=480, 
+    layout="grid"
+)
+app.display()
+
+
--- wip/test_gui.py
+++
@@ -1,3 +0,0 @@
-from guizero import App
-app = App(title="My second GUI app", width=78, height=480, layout="grid")
-app.display()
