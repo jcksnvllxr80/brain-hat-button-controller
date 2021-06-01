8f3aa49eb9fe52d8338299bd690eb4cd73231415
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-31 21:11:09 -0400

    add several buttons to win add funtions to support thee buttons

--- wip/guitkinter_test.py
+++ wip/guitkinter_test.py
@@ -8,31 +8,41 @@ def init(win):
   win.minsize(800, 480)
   btn.pack()
 
-# button callback
-def hello():
-  messagebox.showinfo("Hello", "Pleased to meet you!")
+# button callbacks
+def photo():
+  messagebox.showinfo("photo", "photo button was pressed!")
+
+def select():
+  messagebox.showinfo("select", "select button was pressed!")
+
+def up():
+  messagebox.showinfo("up", "up button was pressed!")
+
+def down():
+  messagebox.showinfo("down", "down button was pressed!")
+
+def left():
+  messagebox.showinfo("left", "left button was pressed!")
+
+def right():
+  messagebox.showinfo("right", "right button was pressed!")
+
+def menu():
+  messagebox.showinfo("menu", "menu button was pressed!")
 
 # create top-level window
 win = Tk()
-
-# Gets the requested values of the height and widht.
-windowWidth = win.winfo_reqwidth()
-windowHeight = win.winfo_reqheight()
-
-# Gets both half the screen width/height and window width/height
-# positionRight = int(win.winfo_screenwidth()/2 - windowWidth/2)
-# positionDown = int(win.winfo_screenheight()/2 - windowHeight/2)
-
 positionRight = 0
 positionDown = 0
-
-
-# Positions the window in the center of the page.
 win.geometry("+{}+{}".format(positionRight, positionDown))
+select_btn = Button(win, text="select", command=select)
+photo_btn = Button(win, text="photo", command=photo)
+up_btn = Button(win, text="up", command=up)
+down_btn = Button(win, text="down", command=down)
+left_btn = Button(win, text="left", command=left
+right_btn = Button(win, text="right", command=right)
+menu_btn = Button(win, text="menu", command=menu)
 
-# create a button
-btn = Button(win, text="Hello", command=hello)
-
-# initialise and start main loop
+# initialise and start main lophoto
 init(win)
 mainloop()
