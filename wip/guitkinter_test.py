190f33cddbc5a6d004fd773d7ebe2afbb9637d80
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-31 23:16:27 -0400

    add a thing to git ignore a lot of progress with tkinter

--- wip/guitkinter_test.py
+++ wip/guitkinter_test.py
@@ -1,6 +1,38 @@
-# Write your code here :-)
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
+import time
+
+class button_with_timer(tk.Button):
+
+  def __init__(self, container, text, command, width, height):
+    self.container = container
+    tk.Button.__init__(self, self.container, text=text, command=command, width=width, height=height)
+    self.bind("<ButtonPress>", self.on_press)
+    self.bind("<ButtonRelease>", self.on_release)
+    self.button_press_time = 0
+    self.button_release_time = 0
+    self.long_press = False
+
+  def on_press(self, button):
+    self.button_press_time = time.time()
+    # self.log("button was pressed at {0}".format(self.button_press_time))
+
+  def on_release(self, button):
+    self.button_release_time = time.time()
+    # self.log("button was released at {0}".format(self.button_release_time))
+    delta = self.button_release_time - self.button_press_time
+    if delta > 0.75:
+      self.long_press = True
+    else:
+      self.long_press = False 
+    self.log("\ndelta:\n{0:.2f}\npress:\n{1}".format(delta, "long" if self.long_press else "short"))
+
+  def log(self, message):
+    now = time.strftime("%I:%M:%S", time.localtime())
+    print("{}\n{}".format(now, message.strip()))
+    text_area.delete('1.0', tk.END)
+    text_area.insert("end", "{}\n{}".format(now, message.strip()))
+    text_area.see("end")
 
 # initialise main window
 def init(win):
@@ -13,43 +45,52 @@ def init(win):
   left_btn.pack()
   right_btn.pack()
   menu_btn.pack()
+  text_area.pack()
 
 # button callbacks
 def photo():
-  messagebox.showinfo("photo", "photo button was pressed!")
+  tk.messagebox.showinfo("photo", "photo button was pressed!")
 
 def select():
-  messagebox.showinfo("select", "select button was pressed!")
+  tk.messagebox.showinfo("select", "select button was pressed!")
 
 def up():
-  messagebox.showinfo("up", "up button was pressed!")
+  tk.messagebox.showinfo("up", "up button was pressed!")
 
 def down():
-  messagebox.showinfo("down", "down button was pressed!")
+  tk.messagebox.showinfo("down", "down button was pressed!")
 
 def left():
-  messagebox.showinfo("left", "left button was pressed!")
+  tk.messagebox.showinfo("left", "left button was pressed!")
 
 def right():
-  messagebox.showinfo("right", "right button was pressed!")
+  tk.messagebox.showinfo("right", "right button was pressed!")
 
 def menu():
-  messagebox.showinfo("menu", "menu button was pressed!")
+  tk.messagebox.showinfo("menu", "menu button was pressed!")
 
 # create top-level window
-win = Tk()
+win = tk.Tk()
+win.attributes('-alpha',0.5)
+left_button_frame = tk.Frame(win)
+left_button_frame.pack(fill=tk.X, side=tk.LEFT)
+right_button_frame = tk.Frame(win)
+right_button_frame.pack(fill=tk.X, side=tk.RIGHT)
+
 positionRight = 0
 positionDown = 0
 win.geometry("+{}+{}".format(positionRight, positionDown))
 
-select_btn = Button(win, text="select", command=select, width=10, justify="left")
-photo_btn = Button(win, text="photo", command=photo, width=10, justify="left")
-up_btn = Button(win, text="up", command=up, width=10, justify="left")
-down_btn = Button(win, text="down", command=down, width=10, justify="left")
-left_btn = Button(win, text="left", command=left, width=10, justify="left")
-right_btn = Button(win, text="right", command=right, width=10, justify="left")
-menu_btn = Button(win, text="menu", command=menu, width=10, justify="left")
+text_area = tk.Text(right_button_frame, height=10, width=8)
+select_btn = button_with_timer(right_button_frame, text="select", command=select, width=8, height=3)
+photo_btn = button_with_timer(right_button_frame, text="photo", command=photo, width=8, height=3)
+up_btn = button_with_timer(left_button_frame, text="up", command=up, width=8, height=3)
+down_btn = button_with_timer(left_button_frame, text="down", command=down, width=8, height=3)
+left_btn = button_with_timer(left_button_frame, text="left", command=left, width=8, height=3)
+right_btn = button_with_timer(left_button_frame, text="right", command=right, width=8, height=3)
+menu_btn = button_with_timer(right_button_frame, text="menu", command=menu, width=8, height=3)
+
 
 # initialise and start main lophoto
 init(win)
-mainloop()
+tk.mainloop()
